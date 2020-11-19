d3741ea58895a70fd7fb4879fac20f5f72f467c7
Phạm Lê Minh Trí <tripham9299@gmail.com>
2020-11-19 16:57:32 +0700

    profile

---
+++ lib/src/views/Profile/fake_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './models/friends.dart';
+final FAKE_FRIENDS = [
+  Friends(id:3 ,name:'Bill Gates', image: NetworkImage('https://www.sageisland.com/wp-content/uploads/2017/06/beat-instagram-algorithm.jpg')),
+  Friends(id:2 ,name:'Donal Trump', image: NetworkImage('https://www.sageisland.com/wp-content/uploads/2017/06/beat-instagram-algorithm.jpg')),
+  Friends(id:1 ,name:'Mark Zuckerberg', image: NetworkImage('https://www.sageisland.com/wp-content/uploads/2017/06/beat-instagram-algorithm.jpg')),
+  Friends(id:5 ,name:'Stephen Hawking', image: NetworkImage('https://www.sageisland.com/wp-content/uploads/2017/06/beat-instagram-algorithm.jpg')),
+  Friends(id:4 ,name:'Steve Jobs', image: NetworkImage('https://www.sageisland.com/wp-content/uploads/2017/06/beat-instagram-algorithm.jpg')),
+  Friends(id:6 ,name:'Barack Obama', image: NetworkImage('https://www.sageisland.com/wp-content/uploads/2017/06/beat-instagram-algorithm.jpg')),
+
+];
---
+++ lib/src/views/Profile/models/friends.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Friends{
+  final int id;
+  final String name;
+  final NetworkImage image;
+  Friends({
+    this.id,
+    this.name,
+    this.image
+  });
+}
